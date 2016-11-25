5226e58aca856b050bee1a00776136075b306699
Aleksandr Karpov <a.karpov@semrush.com>
2016-11-25 17:37:42 +0300

    add some pretty for code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
@@ -7,46 +6,72 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 /**
  * Created by time2die on 06.11.16.
  */
-@SpringBootApplication
 
 public class Main {
-    private static final Logger logger = Logger.getLogger("tRussianBot");
 
     public static void main(String[] args) throws TelegramApiRequestException {
-        TelegramBotsApi tg = new TelegramBotsApi();
         ApiContextInitializer.init();
 
-        tg.registerBot(new MyAmazingBot());
+        new TelegramBotsApi().registerBot(new tRussianBankBot());
     }
 
-    public static class MyAmazingBot extends TelegramLongPollingBot {
+    public static class tRussianBankBot extends TelegramLongPollingBot {
         @Override
         public void onUpdateReceived(Update update) {
-            logger.log(Level.ALL, update.getMessage().getChatId().toString());
-            logger.log(Level.ALL, update.getMessage().getText());
-            if (updateHasCommand(update,"/status")) {
-                if (update.getMessage().getChatId() == 69711013 || update.getMessage().getChatId() == -29036710) {
-
-                    SendMessage message = new SendMessage()
-                            .setChatId(update.getMessage().getChatId())
-                            .setText(GoogleApiClient.getStatus());
-                    try {
-                        sendMessage(message);
-                    } catch (TelegramApiException e) {
-                        e.printStackTrace();
-                    }
+            if (updateHasCommand(update, "/status")) {
+                if (userHasRights(update)) {
+                    sendMessage(update, GoogleApiClient.getStatus());
                 }
+            }else if(updateHasCommand(update, "/search урбанист") && userHasRights(update)){
+                sendMessage(update,"Довольно странно спрашивать информация про урбаниста кода Вы в компании приличных русских людей");
+            } else if (updateStartWithCommand(update, "/search") && userHasRights(update)) {
+                processSearchOperation(update);
+            } else {
+                processElseVariant(update);
             }
         }
 
-        public boolean updateHasCommand(Update update, String message){
-            return  update.hasMessage() && update.getMessage().hasText() && message.equals(update.getMessage().getText().trim().toLowerCase()) ;
+        void processSearchOperation(Update update){
+            String text = null;
+            try {
+                text = update.getMessage().getText().split(" ")[1];
+            } catch (java.lang.ArrayIndexOutOfBoundsException ex) {
+                sendMessage(update, "Следует указать кого вы ищите.\nПример работы: /search урбанист");
+            }
+
+            sendMessage(update, text);
+        }
+
+        void processElseVariant(Update update) {
+            if (userHasRights(update))
+                sendMessage(update, "cant analize:>" + update.getMessage().getText() + "<");
+            else
+                sendMessage(update, "you hasn't right for this command");
+        }
+
+        public void sendMessage(Update update, String text) {
+            SendMessage message = new SendMessage()
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(text);
+            try {
+                sendMessage(message);
+            } catch (TelegramApiException e) {e.printStackTrace();}
+        }
+
+        public boolean updateHasCommand(Update update, String message) {
+            return update.hasMessage() && update.getMessage().hasText() && message.equals(update.getMessage().getText().trim().toLowerCase());
+        }
+
+        public boolean updateStartWithCommand(Update update, String message) {
+            return update.hasMessage() && update.getMessage().hasText() && update.getMessage().getText().toLowerCase().startsWith(message);
+        }
+
+
+        public boolean userHasRights(Update update) {
+            return update.getMessage().getChatId() == 69711013 || update.getMessage().getChatId() == -29036710;
         }
 
         @Override
@@ -60,4 +85,3 @@ public class Main {
         }
     }
 }
-
