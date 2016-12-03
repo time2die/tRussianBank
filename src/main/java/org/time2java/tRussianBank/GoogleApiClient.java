61d59a7913eb5d5d1257692a1f136de0e565a68c
time2die <time2java@gmail.com>
2016-12-03 15:30:16 +0300

    add debts

--- src/main/java/org/time2java/tRussianBank/GoogleApiClient.java
+++ src/main/java/org/time2java/tRussianBank/GoogleApiClient.java
@@ -1,3 +1,5 @@
+package org.time2java.tRussianBank;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
@@ -25,36 +27,9 @@ public class GoogleApiClient {
         String key = conf.getString("key") ;
         String url = "https://sheets.googleapis.com/v4/spreadsheets/"+docId+"/values/A5%3AB7?key="+key;
 
-        HttpClient client = HttpClientBuilder.create().build();
-        HttpGet request = new HttpGet(url);
+        gaAnswer ga = getAnswerFromGA(url) ;
 
-        HttpResponse response = null;
-        try {
-            response = client.execute(request);
-        } catch (IOException e) { e.printStackTrace();}
-
-        System.out.println("-------------------------------------------: "+url);
-
-        BufferedReader rd = null;
-        try {
-            rd = new BufferedReader( new InputStreamReader(response.getEntity().getContent()));
-        } catch (IOException e) { e.printStackTrace(); }
-
-        StringBuffer result = new StringBuffer();
-        String line = "";
-        try {
-            while ((line = rd.readLine()) != null) {
-                result.append(line);
-            }
-        } catch (IOException e) { e.printStackTrace(); }
-
-        ObjectMapper om = new ObjectMapper();
-        gaAnswer ga = null ;
-        try {
-            ga = om.readValue(result.toString() , gaAnswer.class);
-        } catch (IOException e) { e.printStackTrace(); }
-
-        result = new StringBuffer() ;
+        StringBuilder result = new StringBuilder() ;
         for (List<String> iter1 : ga.getValues()) {
             for (String iter2 : iter1) {
                 result.append(iter2) ;
@@ -77,22 +52,24 @@ public class GoogleApiClient {
         String key = conf.getString("key") ;
         String url = "https://sheets.googleapis.com/v4/spreadsheets/"+docId+"/values/%D0%A3%D1%87%D0%B0%D1%81%D1%82%D0%BD%D0%B8%D0%BA%D0%B8!A2%3AL50?key="+key ;
 
+        return  getAnswerFromGA(url) ;
+    }
+
+    private static gaAnswer getAnswerFromGA(String uri){
         HttpClient client = HttpClientBuilder.create().build();
-        HttpGet request = new HttpGet(url);
+        HttpGet request = new HttpGet(uri);
 
         HttpResponse response = null;
         try {
             response = client.execute(request);
         } catch (IOException e) { e.printStackTrace();}
 
-        System.out.println("-------------------------------------------: "+url);
-
         BufferedReader rd = null;
         try {
             rd = new BufferedReader( new InputStreamReader(response.getEntity().getContent()));
         } catch (IOException e) { e.printStackTrace(); }
 
-        StringBuffer result = new StringBuffer();
+        StringBuilder result = new StringBuilder();
         String line = "";
         try {
             while ((line = rd.readLine()) != null) {
@@ -100,8 +77,6 @@ public class GoogleApiClient {
             }
         } catch (IOException e) { e.printStackTrace(); }
         String resultString = StringUtils.replace(result.toString(),"ё","е") ;
-
-        System.out.println("rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrre:\n"+resultString);
 
         ObjectMapper om = new ObjectMapper();
         gaAnswer ga = null ;
